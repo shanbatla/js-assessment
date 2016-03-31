2bee6f89281babc9ffead81dea0d48ea03a58348
shanbatla <shan.batla@gmail.com>
2016-03-31 11:38:57 -0700

    Object-tests complete.

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,23 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects : function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
+    return constructor;
   },
 
   iterate : function(obj) {
+    var result = [];
 
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        result.push(key + ': ' + obj[key]);
+      }
+    }
+
+    return result;
   }
 };
