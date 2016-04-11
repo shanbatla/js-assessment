bd4be33148b5735e116e55e04d93f5dc5431405f
shanbatla <shan.batla@gmail.com>
2016-04-10 18:34:14 -0700

    BestPractices-test complete.

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -8,7 +8,7 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.bestPracticesAnswers = {
   globals : function() {
-    myObject = {
+    var myObject = {
       name : 'Jory'
     };
 
@@ -26,10 +26,10 @@ exports.bestPracticesAnswers = {
   },
 
   parseInt : function(num) {
-    return parseInt(num);
+    return parseInt(num, 10);
   },
 
   identity : function(val1, val2) {
-
+    return val1 === val2;
   }
 };
